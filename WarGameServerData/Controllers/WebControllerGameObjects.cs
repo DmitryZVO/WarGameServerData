ff1da956b06dbf8d1c5f8adfd76be69fa6c68ed6
DmitryZVO <zvonarev@cz71.ru>
2026-02-25 19:11:02 +0300

    111111

--- WarGameServerData/Controllers/WebControllerGameObjects.cs
+++ WarGameServerData/Controllers/WebControllerGameObjects.cs
@@ -42,6 +42,61 @@ public class WebControllerGameObjects : ControllerBase
             return NotFound();
         }
     }
+    [Route("SetGameObjectRelay")]
+    public IActionResult SetGameObjectRelay(string name, [FromBody] JsonObject json)
+    {
+        try
+        {
+            var relays = JsonSerializer.Deserialize<RelayForWrite>(json.ToJsonString());
+            if (relays == null) return NotFound();
+
+            var objs = Core.IoC.Services.GetRequiredService<GameObjects>().Items;
+            lock (objs)
+            {
+                var obj = objs.Find(x => x.Name.Equals(name));
+                if (obj == null)
+                {
+                    obj = new GameObject { LastTime = DateTime.Now, Name = name };
+                    Core.IoC.Services.GetRequiredService<GameObjects>().Items.Add(obj);
+                }
+                obj.RelayValsForWrite = relays;
+                obj.Requests.RelayRewriteLastTime = DateTime.Now;
+            }
+
+            return Ok();
+        }
+        catch (Exception e)
+        {
+            Core.IoC.Services.GetRequiredService<ILogger<WebControllerGameObjects>>().Log(LogLevel.Error, e.ToString());
+            return NotFound();
+        }
+    }
+    [Route("SetGameObjectPtz")]
+    public IActionResult SetGameObjectPtz(string name, byte ptz)
+    {
+        try
+        {
+            var objs = Core.IoC.Services.GetRequiredService<GameObjects>().Items;
+            lock (objs)
+            {
+                var obj = objs.Find(x => x.Name.Equals(name));
+                if (obj == null)
+                {
+                    obj = new GameObject { LastTime = DateTime.Now, Name = name };
+                    Core.IoC.Services.GetRequiredService<GameObjects>().Items.Add(obj);
+                }
+
+                obj.Requests.Ptz = ptz;
+            }
+
+            return Ok();
+        }
+        catch (Exception e)
+        {
+            Core.IoC.Services.GetRequiredService<ILogger<WebControllerGameObjects>>().Log(LogLevel.Error, e.ToString());
+            return NotFound();
+        }
+    }
 
     [Route("GetGameObjectTelem")]
     public IActionResult GetGameObjectTelem(string name)
@@ -106,8 +161,6 @@ public class WebControllerGameObjects : ControllerBase
     [Route("SetCamera")]
     public IActionResult SetCamera(int id, int number, [FromBody] JsonObject json)
     {
-        var w = 640;
-        var h = 480;
         try
         {
             var frame = Convert.FromBase64String(JsonSerializer.Deserialize<CameraVideo>(json.ToJsonString())!.FileBase64);//Convert.FromBase64String(json.ToJsonString());
@@ -122,8 +175,9 @@ public class WebControllerGameObjects : ControllerBase
                     return NotFound();
                 }
 
-                obj.Telem.MBitServerInBytesCounter += frame.Length; // Обновляем счетчик принятых байт на сервер от объекта
-                var rgb = new RgbImage(ImageFormat.Rgb, w, h);
+                obj.Telem.MBitServerInBytesCounter +=
+                    frame.Length; // Обновляем счетчик принятых байт на сервер от объекта
+                var rgb = new RgbImage(ImageFormat.Rgb, CameraFrame.Width, CameraFrame.Height);
                 var s = obj.CamFrames[number].H264Decoder.Decode(frame, 0, frame.Length, true, out var state, ref rgb);
                 if (state != DecodingState.dsErrorFree)
                 {
@@ -133,7 +187,37 @@ public class WebControllerGameObjects : ControllerBase
                 obj.CamFrames[number].Frame.Dispose();
                 var data = rgb.GetBytes();
                 using var mOrig = Mat.FromPixelData(rgb.Height, rgb.Width, MatType.CV_8UC3, data);
-                obj.CamFrames[number].Frame = mOrig.Resize(new Size(CameraFrame.Width, CameraFrame.Height));
+                using var mat4 = mOrig.Resize(new Size(CameraFrame.Width, CameraFrame.Height));
+
+                if (number == 4 | number == 5) // Камеры с круговым обзором, нужна коррекция
+                {
+                    const float xmin = 0.25f;
+                    const float xmax = 0.75f;
+                    const float ymin = 0.20f;
+                    const float ymax = 0.80f;
+                    var srcPoints4 = new List<Point2f>
+                    {
+                        new(CameraFrame.Width * xmin, CameraFrame.Height * ymin),
+                        new(CameraFrame.Width * xmax, CameraFrame.Height * ymin),
+                        new(CameraFrame.Width * xmin, CameraFrame.Height * ymax),
+                        new(CameraFrame.Width * xmax, CameraFrame.Height * ymax)
+                    };
+                    var dstPoints4 = new List<Point2f>
+                    {
+                        new(0, 0),
+                        new(CameraFrame.Width, 0),
+                        new(0, CameraFrame.Height),
+                        new(CameraFrame.Width, CameraFrame.Height)
+                    };
+
+                    using var mat44 = new Mat();
+                    Cv2.WarpPerspective(mat4, mat44, Cv2.GetPerspectiveTransform(srcPoints4, dstPoints4), new Size(CameraFrame.Width, CameraFrame.Height));
+                    obj.CamFrames[number].Frame = mat44.Clone();
+                }
+                else
+                {
+                    obj.CamFrames[number].Frame = mat4.Clone();
+                }
                 rgb.Dispose();
             }
 
