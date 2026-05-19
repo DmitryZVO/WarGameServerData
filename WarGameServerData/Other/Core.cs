cdcc638606f59b7709a8021112c02f4727573a24
Дмитрий Звонарев <zvonarev@dealer-it.ru>
2026-05-19 10:39:26 +0300

    111

--- WarGameServerData/Other/Core.cs
+++ WarGameServerData/Other/Core.cs
@@ -49,7 +49,7 @@ internal class Core
         var radio = IoC.Services.GetRequiredService<ZvoRadio>();
         //radio.AddRadioHead(ZvoRadio.TransferMode.MaxRange, 80, [0, 0, 9, 0, 4, 0, 0, 0, 2]); // потолок 802.11b
         //radio.AddRadioHead(ZvoRadio.TransferMode.MaxRange, 20, [0, 0, 9, 0, 4, 0, 0, 0, 2]); // потолок 802.11b
-        radio.AddRadioHead(ZvoRadio.TransferMode.MaxRange, 2, [0, 0, 11, 0, 0, 0, 8, 0, 15, 12, 0]); // потолок 802.11g
+        radio.AddRadioHead(ZvoRadio.TransferMode.MaxRange, 5, [0, 0, 11, 0, 0, 0, 8, 0, 15, 12, 0]); // потолок 802.11g
         //radio.AddRadioHead(ZvoRadio.TransferMode.MaxRange, 85, [0, 0, 11, 0, 0, 0, 8, 0, 15, 12, 0]); // потолок 802.11n
         radio.StartAsync();
 
