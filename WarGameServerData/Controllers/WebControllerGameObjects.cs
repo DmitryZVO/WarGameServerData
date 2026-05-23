a9e710262e562c1ac61ceacd40f714ad8bf81c12
Дмитрий Звонарев <zvonarev@dealer-it.ru>
2026-05-23 11:02:39 +0300

    111

--- WarGameServerData/Controllers/WebControllerGameObjects.cs
+++ WarGameServerData/Controllers/WebControllerGameObjects.cs
@@ -92,11 +92,11 @@ public class WebControllerGameObjects : ControllerBase
                 item.Telem.MbitsZvoGroundToWaterSend = zvo.ApRadioBytesSend * 8.0f / 1_000_000.0f;
                 item.Telem.MbitsZvoGroundToWaterRecv = zvo.ApRadioBytesRecv * 8.0f / 1_000_000.0f;
                 item.Telem.RepeaterAlive = zvo.Repeater.Alive;
-                item.Telem.QueueRepeaterSend = (byte)Math.Min(zvo.Repeater.SendQueue, 255);
-                item.Telem.MbitsRepeaterGroundToWaterRecv = zvo.Repeater.GtoWbytesRecv * 8.0f / 1_000_000.0f;
-                item.Telem.MbitsRepeaterWaterToGroundRecv = zvo.Repeater.WtoGbytesRecv * 8.0f / 1_000_000.0f;
-                item.Telem.QualityRepeaterGroundToWaterRecv = zvo.Repeater.GtoWpacketsRecv / 10.0f;
-                item.Telem.QualityRepeaterWaterToGroundRecv = zvo.Repeater.WtoGpacketsRecv / 10.0f;
+                item.Telem.QueueRepeaterSend = zvo.Repeater.Alive ? (byte)Math.Min(zvo.Repeater.SendQueue, 255) : (byte)0;
+                item.Telem.MbitsRepeaterGroundToWaterRecv = zvo.Repeater.Alive ? zvo.Repeater.GtoWbytesRecv * 8.0f / 1_000_000.0f : 0.0f;
+                item.Telem.MbitsRepeaterWaterToGroundRecv = zvo.Repeater.Alive ? zvo.Repeater.WtoGbytesRecv * 8.0f / 1_000_000.0f : 0.0f;
+                item.Telem.QualityRepeaterGroundToWaterRecv = zvo.Repeater.Alive ? zvo.Repeater.GtoWpacketsRecv / 10.0f : 0.0f;
+                item.Telem.QualityRepeaterWaterToGroundRecv = zvo.Repeater.Alive ? zvo.Repeater.WtoGpacketsRecv / 10.0f : 0.0f;
                 var jsonStr = JsonSerializer.Serialize(item.Telem);
                 return Ok(jsonStr);
             }
